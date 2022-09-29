8008d8483b529b283059ced13e3f8a07e04c19d0
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-09-29 15:20:01 -0500

    Updated context

--- index.js
+++ index.js
@@ -21,12 +21,15 @@ try {
   const cloudflareAccountId = core.getInput('cloudflareAccountId')
   const cloudflareApiToken = core.getInput('cloudflareApiToken')
 
+  const {context} = github
+
   const results = await fetch('https://workers.do/api/deploy', {
     method: 'POST',
     body: { 
+      context,
+      worker,
       cloudflareAccountId,
       cloudflareApiToken,
-      worker,
     },
   }).then(res => res.json()).catch(({name, message, stack}) => ({ error: {name, message, stack}}))
   
