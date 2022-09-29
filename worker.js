5bf09a486a1d4234f6c844e56187cb1d8dfd2b0d
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-09-29 17:27:15 -0500

    Pinned apis.do version

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-import { apis, json } from 'https://pkg.do/apis.do'
+import { apis, json } from 'https://pkg.do/apis.do@0.1.4'
 
 export default {
     fetch: () => new Response(JSON.stringify({apis}))
