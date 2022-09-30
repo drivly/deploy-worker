a4336734452dc39b9ad48f60bcdd2f77fec82025
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-09-30 06:42:18 -0500

    Swapping back

--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
-import { json, apis } from 'https://pkg.do/apis.do'
+// import { json, apis } from 'https://pkg.do/apis.do'
+import { json } from 'https://pkg.do/itty-router-helpers'
 
 export default {
-    fetch: req => json({ hello: 'https://workers.do/api', apis })
+    fetch: req => json({ hello: 'https://workers.do/api' })
 }
