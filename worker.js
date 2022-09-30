1e2b4a3dabaefe751b99b635025739161006505c
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-09-30 06:28:47 -0500

    Cleanup

--- worker.js
+++ worker.js
@@ -1,6 +1,5 @@
-// import { json } from 'https://pkg.do/apis.do@0.1.4'
+import { json } from 'https://pkg.do/apis.do@0.1.4'
 
 export default {
-    fetch: req => new Response(JSON.stringify({ hello: req.cf.city }, null, 2))
+    fetch: req => json({ hello: 'world' })
 }
-
