20e339154faa1a51020ddda5f3d272d18bb43ca8
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-09-30 06:43:43 -0500

    Fixed package reference

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 // import { json, apis } from 'https://pkg.do/apis.do'
-import { json } from 'https://pkg.do/itty-router-helpers'
+import { json } from 'https://pkg.do/itty-router-extras'
 
 export default {
     fetch: req => json({ hello: 'https://workers.do/api' })
