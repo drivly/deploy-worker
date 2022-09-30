0057cd04778ca1ef2a3b35acba4e7e08436f2ed9
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-09-30 06:58:41 -0500

    Trying again with newer version

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-import { json } from 'https://pkg.do/apis.do@0.1.7'
+import { json } from 'https://pkg.do/apis.do@0.1.8'
 // import { json } from 'https://pkg.do/itty-router-extras'
 
 export default {
