b323baae00d0e7c9869442e6062646469dde9581
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-09-30 06:46:13 -0500

    Reverting to publish action

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
-import { json } from 'https://pkg.do/apis.do@0.1.6'
-// import { json } from 'https://pkg.do/itty-router-extras'
+// import { json } from 'https://pkg.do/apis.do@0.1.6'
+import { json } from 'https://pkg.do/itty-router-extras'
 
 export default {
     fetch: req => json({ hello: 'https://workers.do/api' })
